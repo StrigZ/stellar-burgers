7da05b10e3c236fb0074a75afa1071e172b76795
StrigZ <mirgasan200@gmail.com>
2026-04-08 16:54:58 +0300

    Use isActive NavLink prop to change icons state

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -25,10 +25,14 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
                 : styles.link
             }
           >
-            <BurgerIcon type={'primary'} />
-            <p className='text text_type_main-default ml-2 mr-10'>
-              Конструктор
-            </p>
+            {({ isActive }) => (
+              <>
+                <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
+                <p className='text text_type_main-default ml-2 mr-10'>
+                  Конструктор
+                </p>
+              </>
+            )}
           </NavLink>
           <NavLink
             to={'/feed'}
@@ -36,8 +40,14 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
               isActive ? `${styles.link} ${styles.link_active}` : styles.link
             }
           >
-            <ListIcon type={'primary'} />
-            <p className='text text_type_main-default ml-2'>Лента заказов</p>
+            {({ isActive }) => (
+              <>
+                <ListIcon type={isActive ? 'primary' : 'secondary'} />
+                <p className='text text_type_main-default ml-2'>
+                  Лента заказов
+                </p>
+              </>
+            )}
           </NavLink>
         </div>
         <div className={styles.logo}>
@@ -49,10 +59,14 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
             isActive ? `${styles.link} ${styles.link_active}` : styles.link
           }
         >
-          <ProfileIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>
-            {userName || 'Личный кабинет'}
-          </p>
+          {({ isActive }) => (
+            <>
+              <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>
+                {userName || 'Личный кабинет'}
+              </p>
+            </>
+          )}
         </NavLink>
       </nav>
     </header>
