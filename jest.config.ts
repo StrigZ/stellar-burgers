562a01477c066817de82a1099763e2fa20b24ce2
StrigZ <mirgasan200@gmail.com>
2026-05-04 18:58:49 +0300

    Make jest use aliases from tsconfig

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,8 @@
  */
 
 import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -92,7 +94,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
