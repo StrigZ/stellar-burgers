4a700410b62791d95bad6c7f4a463c40e530ff57
StrigZ <mirgasan200@gmail.com>
2026-05-05 18:14:23 +0300

    Add unit test for order slice

---
+++ src/services/order/order.test.ts
@@ -0,0 +1,116 @@
+import { describe, test, expect, beforeEach } from '@jest/globals';
+import { orderBurgerApi, getOrderByNumberApi } from '@api';
+import { configureStore } from '@reduxjs/toolkit';
+import { getOrderById, makeOrder } from './order-actions';
+import { rootReducer } from '../store';
+import bun from '../../../mocks/bun.json';
+import ingredients from '../../../mocks/ingredients.json';
+
+jest.mock('@api', () => ({
+  getOrderByNumberApi: jest.fn(),
+  orderBurgerApi: jest.fn()
+}));
+
+const mockedGetOrderByNumberApi = getOrderByNumberApi as jest.MockedFunction<
+  typeof getOrderByNumberApi
+>;
+const mockedOrderBurgerApi = orderBurgerApi as jest.MockedFunction<
+  typeof orderBurgerApi
+>;
+
+function createTestStore() {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState: {
+      burgerConstructor: {
+        bun,
+        ingredients
+      }
+    }
+  });
+}
+
+describe('order thunks', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('при вызове экшена request isLoading и isOrdering меняются на true', () => {
+    mockedGetOrderByNumberApi.mockImplementation(() => new Promise(() => {}));
+    mockedOrderBurgerApi.mockImplementation(() => new Promise(() => {}));
+    const store = createTestStore();
+
+    store.dispatch(getOrderById(0));
+    store.dispatch(makeOrder());
+
+    const state = store.getState().order;
+    expect(state.loading).toBe(true);
+    expect(state.isOrdering).toBe(true);
+  });
+
+  test('при успешном выполнении данные сохраняются, флаги сбрасываются', async () => {
+    const orderData: Awaited<ReturnType<typeof getOrderByNumberApi>> = {
+      success: true,
+      orders: [
+        {
+          _id: '',
+          status: '',
+          name: '',
+          createdAt: '',
+          updatedAt: '',
+          number: 1,
+          ingredients: []
+        }
+      ]
+    };
+    const newOrderData: Awaited<ReturnType<typeof orderBurgerApi>> = {
+      success: true,
+      name: 'test',
+      order: {
+        _id: '',
+        status: '',
+        name: '',
+        owner: { name: '', email: '', createdAt: '', updatedAt: '' },
+        createdAt: '',
+        updatedAt: '',
+        number: 1,
+        price: 1
+      }
+    };
+
+    mockedGetOrderByNumberApi.mockResolvedValue(orderData);
+    mockedOrderBurgerApi.mockResolvedValue(newOrderData);
+
+    const store = createTestStore();
+    await store.dispatch(getOrderById(0));
+    await store.dispatch(makeOrder());
+
+    const state = store.getState().order;
+
+    expect(state.loading).toBe(false);
+    expect(state.error).toBeNull();
+    expect(state.order).toEqual(orderData.orders[0]);
+
+    expect(state.isOrdering).toBe(false);
+    expect(state.orderingError).toBeNull();
+    expect(state.orderModalData).toEqual(newOrderData.order);
+  });
+
+  test('при ошибке флаги сбрасываются, ошибки сохраняются', async () => {
+    mockedGetOrderByNumberApi.mockRejectedValue(new Error('Rejected'));
+    mockedOrderBurgerApi.mockRejectedValue(new Error('Rejected'));
+
+    const store = createTestStore();
+    await store.dispatch(getOrderById(0));
+    await store.dispatch(makeOrder());
+
+    const state = store.getState().order;
+
+    expect(state.loading).toBe(false);
+    expect(state.error).toBe('Rejected');
+    expect(state.order).toBeNull();
+    expect(state.isOrdering).toBe(false);
+    expect(state.orderingError).toBe('Rejected');
+    expect(state.orderModalData).toBeNull();
+  });
+});
