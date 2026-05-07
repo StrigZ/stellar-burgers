05094b1263727f14fe57c0c7d41a81d2b9d2e121
StrigZ <mirgasan200@gmail.com>
2026-05-07 17:17:41 +0300

    Fix store tests

--- src/services/store.test.ts
+++ src/services/store.test.ts
@@ -1,15 +1,35 @@
 import { expect, test } from '@jest/globals';
 import { rootReducer } from './store';
+import { burgerConstructorSlice } from './burger-constructor/burger-constructor-slice';
+import { orderSlice } from './order/order-slice';
+import { ingredientsSlice } from './ingredients/ingredients-slice';
+import { feedSlice } from './feed/feed-slice';
+import { userOrdersSlice } from './user-orders/user-orders-slice';
+import { userSlice } from './user/user-slice';
 
-test('rootReducer должен правильно инициализироваться', () => {
-  const initialState = rootReducer(undefined, { type: '@@INIT' });
+test('initializes the state correctly', () => {
+  const initAction = { type: '@@INIT' };
+  const state = rootReducer(undefined, initAction);
 
-  expect(initialState).toBeDefined();
+  expect(state).toEqual({
+    burgerConstructor: burgerConstructorSlice.reducer(undefined, initAction),
+    order: orderSlice.reducer(undefined, initAction),
+    ingredients: ingredientsSlice.reducer(undefined, initAction),
+    feed: feedSlice.reducer(undefined, initAction),
+    userOrders: userOrdersSlice.reducer(undefined, initAction),
+    user: userSlice.reducer(undefined, initAction)
+  });
+});
 
-  expect(initialState).toHaveProperty('user');
-  expect(initialState).toHaveProperty('userOrders');
-  expect(initialState).toHaveProperty('feed');
-  expect(initialState).toHaveProperty('ingredients');
-  expect(initialState).toHaveProperty('order');
-  expect(initialState).toHaveProperty('burgerConstructor');
+test('handles unknown action correctly', () => {
+  const fakeAction = { type: 'UNKNOWN_ACTION' };
+  const state = rootReducer(undefined, fakeAction);
+  expect(state).toEqual({
+    burgerConstructor: burgerConstructorSlice.reducer(undefined, fakeAction),
+    order: orderSlice.reducer(undefined, fakeAction),
+    ingredients: ingredientsSlice.reducer(undefined, fakeAction),
+    feed: feedSlice.reducer(undefined, fakeAction),
+    userOrders: userOrdersSlice.reducer(undefined, fakeAction),
+    user: userSlice.reducer(undefined, fakeAction)
+  });
 });
